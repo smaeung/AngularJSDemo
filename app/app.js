f548b0e7890b95c08650042d934fa18ef3340775
Sungho Maeung <smaeung@gmail.com>
2015-04-28 23:59:51 -0700

    reversed back

--- app/app.js
+++ app/app.js
@@ -8,5 +8,5 @@ angular.module('myApp', [
   'myApp.version'
 ]).
 config(['$routeProvider', function($routeProvider) {
-  $routeProvider.otherwise({redirectTo: '/view1_hello'});
+  $routeProvider.otherwise({redirectTo: '/view1'});
 }]);
